1f152fccd8be5fa9168b8c0fe42e77f05cc8e3db
voldyabishek <aa9177954@gmail.com>
2024-12-16 16:28:27 +0530

    Add files via upload

---
+++ js4thclassloop.js
@@ -0,0 +1,33 @@
+//while loop
+    let a=5
+    while(a>0)
+    {
+        console.log(a)
+        a--
+    }
+
+   excercise
+let a=4
+  let b=1
+    while(a>b)
+    {
+        console.log(a)
+        a--
+    }
+
+
+//do..while loop
+   i=18
+ 
+ do{
+    i=i-1
+    console.log(i);
+    
+} while(i>10) 
+   
+
+ //for loop
+       for(x=0;x<=6;x++)
+       {
+        console.log(x)
+       }
