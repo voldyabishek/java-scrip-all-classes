63e8a98fe2fb18672afb7ad8a668428a5c5deb72
voldyabishek <aa9177954@gmail.com>
2024-12-24 16:48:37 +0530

    Update train.js

--- js train/js/train.js
+++ js train/js/train.js
@@ -1,22 +1,52 @@
-const view =document.getElementById("view1");
-console.log(view);
+// const view =document.getElementById("view1");
+// console.log(view);
 
-//div
-const divs = view.querySelectorAll("div")
-console.log(view);
+// //div
+// const divs = view.querySelectorAll("div")
+// console.log(view);
 
-const evenDivs = view.querySelectorAll("div:nth-of-type(even)")
-console.log(evenDivs);
+// const evenDivs = view.querySelectorAll("div:nth-of-type(even)")
+// console.log(evenDivs);
 
-//ensure there are enough elements before accessing
+// //ensure there are enough elements before accessing
 
-evenDivs[4].style.backgroundcolor="red"
+// evenDivs[4].style.backgroundcolor="red"
 
 
 
-for (let i = 0; i < evenDivs.length; i++)
-     {
-    evenDivs[i].style.backgroundColor = "Green";
-    evenDivs[i].style.width = "150px";
+// for (let i = 0; i < evenDivs.length; i++)
+//      {
+//     evenDivs[i].style.backgroundColor = "Green";
+//     evenDivs[i].style.width = "150px";
    
-     }
+//      }
+
+
+// js train try1
+     const box= document.getElementById("boxes1")
+     console.log(box)
+  //div//
+     const divs=box.querySelectorAll("div")
+     console.log(box);
+
+     function add(a,b)
+       {
+        const el=document.createElement("div")
+        el.textContent="div "+b
+        a.appendChild(el)
+       }
+
+       for(let i=5;i<10;i++)
+          {
+            add(box,i)
+          }
+
+          const evendivs=box.querySelectorAll("div:nth-of-type(even)")
+          
+          console.log(evendivs);
+
+          for(let i=0;i< evendivs.length;i++)
+          {
+            evendivs[i].computedStyleMap.backgroundcolor="yelllow"
+            evendivs[i].style.height = "150px";
+          }
