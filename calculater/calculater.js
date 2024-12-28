74a0b896022f2df2dc0ae102f93f625968323a58
voldyabishek <aa9177954@gmail.com>
2024-12-28 17:11:53 +0530

    Add files via upload

--- calculater/calculater.js
+++ calculater/calculater.js
@@ -1,64 +1,74 @@
 
-function button1(btn)
-{
-    let input = document.querySelector("input")
-    let a;
-    switch(btn)
-    {
-        case '1':
-            a= '1'
-            break;
-        case '2':
-            a= '2'
-            break;
-        case '3':
-           a='3'
-           break;
-        case'4':
-           a='4'
-           break;
-        case'5':
-          a='5'   
-          break;
-        case'6':
-           a='6'
-           break;
-        case'7':
-           a='7'
-           break;
-       case'8':
-           a='8'
-           break;
-      case'9':
-           a='9'
-           break;
-      case'0':
-           a='0'
-           break;  
+// function button1(btn)
+// {
+//     let input = document.querySelector("input")
+//     let a;
 
-        case '+':
-            a = '+'
-            break;
-        case '-':
-            a='-'
-            break;
-        case'*':
-          a='*'
-          break;
-        case'/':
-          a='/'
-          break;
-       
-        case'AC':
-           a='clear'
-           break;
-    }
-    input.value=input.value+a
-
-
+function result() {
+    let name= document.querySelector("#result1")
+    name.value=""
 }
-function submit()
-{
+function show(op)
+    {
+        let name= document.querySelector("#result1")
+    
+switch (op)
+ {
+    case '1':
+        a = '1'
+        break;
+    case '2':
+        a = '2'
+        break;
+    case '3':
+        a = '3'
+        break;
+    case '4':
+        a = '4'
+        break;
+    case '5':
+        a = '5'
+        break;
+    case '6':
+        a = '6'
+        break;
+    case '7':
+        a = '7'
+        break;
+    case '8':
+        a = '8'
+        break;
+    case '9':
+        a = '9'
+        break;
+    case '0':
+        a = '0'
+        break;
+
+    case '+':
+        a = '+'
+        break;
+    case '-':
+        a = '-'
+        break;
+    case '*':
+        a = '*'
+        break;
+    case '/':
+        a = '/'
+        break;
+
+    case 'AC':
+        a = 'clear'
+        break;
+}
+name.value = name.value + a
+
+    }
+
+function submit() {
     let input = document.querySelector("input")
-    input.value= eval(input.value)
+    alert(eval(input.value))
+    // input.value = eval(input.value)
+
 }
