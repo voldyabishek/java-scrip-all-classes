fc10008129d5c70ec8aa10ad0b8c4235128a7f0e
voldyabishek <aa9177954@gmail.com>
2025-01-11 17:11:57 +0530

    Add files via upload

---
+++ game2(hand cricket)/game2.js
@@ -0,0 +1,16 @@
+
+let playconfirm=confirm("let start")//confirm na pop up msg la ok(or)cancel button automatic ahh konduvaruthu
+
+if(playconfirm)
+{
+    let playerone=promt("1run or four or six")//promt na input box la ok button click pannuvaruthu
+    if(player1){
+        let player=playerone.trim().toLowercase();
+        if(player=="run1"||player=="four"||player=="six")
+        {
+            let computer1=Math.floor(Math.random()*3)+1;//random na 0,1,2,3 random ahh ethachum oru numper ahh select panikum computer
+            let computer=computer1===1?"run1":computer===2?"four":
+        }
+    }
+
+}
